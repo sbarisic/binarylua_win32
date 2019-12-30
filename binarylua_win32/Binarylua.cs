5dac94bc67cc7c14fd46739a33008c7de6a47e41
Saša Barišić <cartman300@net.hr>
2019-12-30 23:31:25 +0100

    Nuget package update

--- binarylua_win32/Binarylua.cs
+++ binarylua_win32/Binarylua.cs
@@ -12,14 +12,18 @@ namespace binarylua_win32 {
 
 		[DllExport(ExportName = "gmod13_open", CallingConvention = CallingConvention.Cdecl)]
 		public static void Open(IntPtr L) {
-			Lua.PushCFunction(L, (LL) => {
-				int Len = 0;
-				char* Bytecode = (char*)Lua.ToLString(L, -1, new IntPtr(&Len)).ToPointer();
-				if (Lua.LoadBuffer(LL, Bytecode, Len, "binarychunk") != 0)
-					return 0;
-				return 1;
-			});
+			Lua.PushCFunction(L, LuaBinaryLoad);
 			Lua.SetField(L, Lua.GLOBALSINDEX, "binaryload");
+		}
+
+		static int LuaBinaryLoad(IntPtr LL) {
+			int Len = 0;
+			char* Bytecode = (char*)Lua.ToLString(LL, -1, new IntPtr(&Len)).ToPointer();
+
+			if (Lua.LoadBuffer(LL, Bytecode, Len, "binarychunk") != 0)
+				return 0;
+
+			return 1;
 		}
 
 		[DllExport(ExportName = "gmod13_close", CallingConvention = CallingConvention.Cdecl)]
